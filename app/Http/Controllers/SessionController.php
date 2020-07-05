c963e619e9ffd464209bf91b62fa68647579c0b1
MkDierz <msyibbran24@gmail.com>
2020-07-05 12:38:24 +0700

    create regist and session backend

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        if (auth()->attempt(request(['email', 'password'])) == false) {
+            return back()->withErrors([
+                'message' => 'The email or password is incorrect, please try again'
+            ]);
+        }
+
+        return redirect()->to('/games');
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect()->to('/games');
+    }
 }
